be256345c7197c35065c394e0c68c4fba9766bd8
Yaroslaff Fedin <me@inviz.ru>
2011-03-11 11:00:25 +0800

    Add dialog class. Handle submissions and cancel buttons

--- Source/Body/Dialog.js
+++ Source/Body/Dialog.js
@@ -25,6 +25,34 @@ LSD.Mobile.Body.Dialog = new Class({
   
   options: {
     classes: ['page', 'dialog'],
+    nodeType: 1,
+    events: {
+      _dialog: {
+        element: {
+          'click:relay(.cancel)': 'cancel'
+        },
+        expected: {
+          form: {
+            success: 'submit'
+          }
+        }
+      }
+    }
+  },
+  
+  initialize: function() {
+    this.parent.apply(this, arguments);
+    this.addPseudo('submittable');
+  },
+  
+  cancel: function() {
+    this.hide();
+    this.fireEvent('cancel', arguments);
+  },
+  
+  submit: function() {
+    this.hide();
+    this.fireEvent('submit', arguments);
   },
   
   hidden: true
